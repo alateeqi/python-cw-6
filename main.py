f02b409089cc6c35dbb4ce260e71f3cfa9187399
alateeqi <jadm20066@gmail.com>
2023-09-06 20:38:35 +0300

    Update main.py
    class work 6

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 # write your code here
+person = {
+"name":"jana",
+'age':17,
+"hobbies":["drawnig","swimming","tennis","cookng"] 
+}
+print(person["name"])
+print(person["age"])
+
+person["age"]="200"
+person["country"]=" kuwait"
+print(person)
+print(len(person))
+
+person["hobbies"].append("singing")
+
+def checking_hobbies(person):
+    
+    if len(person["hobbies"]) >= 3:
+        print("wow you are amazing")
+
+checking_hobbies(person)
